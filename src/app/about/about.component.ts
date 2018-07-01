81452cc3e6286da8bdf093d24f6a3693f424d59b
vivek1996 <mvivek3112@gmail.com>
2018-07-01 22:35:01 +0530

    Added loader

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
+export class AboutComponent implements OnInit, AfterContentInit {
+  constructor(private spinner: NgxSpinnerService) {}
 
   ngOnInit() {
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 1500);
   }
-
+  ngAfterContentInit() {
+    // this.spinner.hide();
+  }
 }
--- src/app/all-region/all-region.component.ts
+++ src/app/all-region/all-region.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-all-region',
   templateUrl: './all-region.component.html',
   styleUrls: ['./all-region.component.scss']
 })
 export class AllRegionComponent implements OnInit {
-
-  constructor() { }
+  constructor(private spinner: NgxSpinnerService) {}
 
   ngOnInit() {
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 1500);
   }
-
 }
