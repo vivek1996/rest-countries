132f2b1ddeb26b217223af1a19f49779b05a83e2
vivek1996 <mvivek3112@gmail.com>
2018-07-01 22:42:56 +0530

    loader for app-shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit, AfterContentChecked {
+  constructor(private spinner: NgxSpinnerService) {
+    this.spinner.show();
+  }
+  ngOnInit() {}
+  ngAfterContentChecked() {
+    // setTimeout(() => {
+    //   this.spinner.hide();
+    // }, 1500);
+    this.spinner.hide();
+  }
 }
