e2ef74c6a12ce1b983f768b126b27670cd213e40
vivek1996 <mvivek3112@gmail.com>
2018-07-03 11:03:37 +0530

    single country initial

--- src/app/single-country/single-country.component.ts
+++ src/app/single-country/single-country.component.ts
@@ -1,19 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../countries.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-single-country',
   templateUrl: './single-country.component.html',
-  styleUrls: ['./single-country.component.scss']
+  styleUrls: ['./single-country.component.scss'],
+  providers: [Location]
 })
 export class SingleCountryComponent implements OnInit {
-  constructor(private spinner: NgxSpinnerService) {}
+  public country;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: CountriesService,
+    private location: Location,
+    private spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit() {
     this.spinner.show();
     setTimeout(() => {
-      console.log('spinner 1');
       this.spinner.hide();
     }, 5000);
+    // get country
+    this.route.params.subscribe(routeParams => {
+      console.log(routeParams);
+      this.http.getSingleCountry(routeParams.name).subscribe(
+        data => {
+          this.country = data;
+          console.log(this.country);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    });
+  }
+
+  // go back
+  public goBackToPreviousPage() {
+    this.location.back();
   }
 }
